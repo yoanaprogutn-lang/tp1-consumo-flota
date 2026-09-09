c60a25ec409f80ae5a85488969287d8e630a849d
yoanaprogutn-lang <yoanaprog.utn@gmail.com>
2026-09-09 00:13:57 -0300

    Arreglando codigo, modificando informe de salida, modificando calculos para informe completo segun consigna, comentando codigo erroneo respecto de la consigna

--- Flota.java
+++ Flota.java
@@ -3,68 +3,75 @@ public class Flota {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final double PRECIO_LITRO = 1250.0;
+        final double CAPACIDAD_TAQUE = 400.0;
         String continuarPrograma;
-
-        do{
-            int cantidadViajes;
+        int cantidadViajes;
 
         do {
             System.out.print("ingrese la cantidad de viajes a procesar (mayor a cero): ");
-             cantidadViajes = scanner.nextInt();
-        } while (cantidadViajes <=0);
-         int totalKm = 0;
-         double totalLitros = 0;
-         double maxConsumo = -1;
-         String patenteMaxConsumo = "";
-      for (int i = 1; i <= cantidadViajes; i++) {
-        System.out.println("\n---Datos del Viaje #" + i + "---");
-        System.out.print("Ingrese la pantente:  ");
-        scanner.nextLine();
-        String patente = scanner.nextLine();
+            cantidadViajes = scanner.nextInt();
 
-        System.out.print("Ingrese kilometraje inicial;  ");
-        int kmInicial = scanner.nextInt();
+            } while (cantidadViajes <= 0);
+            int totalKm = 0;
+            double totalLitros = 0;
+            double maxConsumo = -1;
+            String patenteMaxConsumo = "";
+            for (int i = 1; i <= cantidadViajes; i++) {
+                System.out.println("\n---Datos del Viaje #" + i + "---");
+                System.out.print("Ingrese la pantente:  ");
+                scanner.nextLine();
+                String patente = scanner.nextLine();
 
-        System.out.print("Ingrese Kilometraje final:  ");
-        int kmFinal = scanner.nextInt();
+                System.out.print("Ingrese kilometraje inicial;  ");
+                int kmInicial = scanner.nextInt();
 
-        System.out.print("Ingrese litros cargados:  ");
-        double litros = scanner.nextDouble();
+                System.out.print("Ingrese Kilometraje final:  ");
+                int kmFinal = scanner.nextInt();
 
-        int kmRecorridos = kmFinal - kmInicial;
-        double consumo100km = (litros / kmRecorridos) * 100;
+                System.out.print("Ingrese litros cargados:  ");
+                double litros = scanner.nextDouble();
 
-        totalKm += kmRecorridos;
-        totalLitros += litros;
+                double kmRecorridos = kmFinal - kmInicial;
+                double consumo100km = (litros / kmRecorridos) * 100;
+                double rendimiento = kmRecorridos / litros;
+                double costoTotal = litros * PRECIO_LITRO;
+                double costoPorKM = costoTotal / kmRecorridos;
+                double porcentajeTanque = (litros / CAPACIDAD_TAQUE) * 100;
 
-             if (consumo100km > maxConsumo) {
+
+
+                /*        if (consumo100km > maxConsumo) {
                  maxConsumo = consumo100km;
                  patenteMaxConsumo = patente;
-             }
+                }
 
-      }
+                }
 
-        double costoTotal = totalLitros * PRECIO_LITRO;
-        double  promedioConsumoFlota = (totalLitros / totalKm) + 100;
+                double costoTotal = totalLitros * PRECIO_LITRO;
+                 double  promedioConsumoFlota = (totalLitros / totalKm) + 100;*/
 
-        System.out.println("\n =====================================");
-        System.out.println("      INFORME CONSOLIDADO DE FLOTA      ");
-        System.out.println("\n =====================================");
-        System.out.printf("    Total de viajes procesados  :  %10d%n", cantidadViajes);
-        System.out.printf("    Total kilometros recorridos :  %10d km%n", totalKm);
-        System.out.printf("    Total de litros consumidos  :  %10.2f L%n", totalLitros );
-        System.out.printf("    Costo total de combustible  :  $%9.2fd%n", costoTotal);
-        System.out.printf("    Consumo promedio Flota      :   %10.2f L/100km%n", promedioConsumoFlota);
-        System.out.println("    -------------------------------------");
-        System.out.printf("    Camion de mayor consumo     : %s (%.2f L/100km)%n", patenteMaxConsumo, maxConsumo);
-        System.out.println("\n =====================================");
+                System.out.println("\n =====================================");
+                System.out.println("      INFORME CONSOLIDADO          ");
+                System.out.println("\n =====================================");
+                System.out.printf("     CAMION PATENTE :  %s%n", patente);
+                System.out.printf("    Total kilometros recorridos :  %,10.2f km%n", kmRecorridos);
+                System.out.printf("    Total de litros consumidos  :  %,10.2f L%n", consumo100km);
+                System.out.printf("    Costo total de combustible  :  $%9.2fd%n", costoTotal);
+                System.out.printf("    Costo por kilometro         :  $ %,9.2f%n", costoPorKM);
+                System.out.printf("    Porcentaje de tanque        :  $ %,10.2f %% %n", costoPorKM);
 
-        System.out.println("\n¿Desea iniciar otra ejecucion/comparacion? (C para continuar / S para salir): ");
-            continuarPrograma = scanner.nextLine().trim();
+                System.out.println("    -------------------------------------");
+                /*System.out.printf("    Camion de mayor consumo     : %s (%.2f L/100km)%n", patenteMaxConsumo, maxConsumo);
+                System.out.println("\n =====================================");
 
-        } while (continuarPrograma.equalsIgnoreCase("C"));
+                System.out.println("\n¿Desea iniciar otra ejecucion/comparacion? (C para continuar / S para salir): ");
+                 continuarPrograma = scanner.nextLine().trim();
 
-        System.out.println("\n¡Gracias por usar el sistema de gestión de flota!");
-        scanner.close();
+                } while (continuarPrograma.equalsIgnoreCase("C"));*/
+
+                System.out.println("\n¡Gracias por usar el sistema de gestión de flota!");
+                scanner.close();
+            }
+
     }
 }
